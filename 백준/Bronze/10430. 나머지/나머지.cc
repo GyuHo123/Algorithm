e26dbb4055e1258448648f2e48b896c39937b1e8
KIM GYUHO <kyuhokim12@gmail.com>
2022-12-27 20:36:13 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int A,B,C;
+    
+    cin >> A >> B >> C;
+    
+    while(1)
+        if(2<= A,B,C <=10000)
+            break;
+        else
+            continue;
+   
+    cout << (A+B)%C << endl;
+    cout << ((A%C) + (B%C))%C << endl;
+    cout << (A*B)%C << endl;
+    cout << ((A%C) * (B%C))%C << endl;
+        
+}
