38c1df4838059123fb8cfc2101952060d7ade759
KIM GYUHO <kyuhokim12@gmail.com>
2023-02-19 00:18:52 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int dp[10001];
+int weight[10001];
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        cin >> weight[i];
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++)
+        dp[i] = max(dp[i], dp[i - j] + weight[j]);
+    }
+    cout << dp[n];
+}
