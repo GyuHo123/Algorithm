fc4d1f6cb3b3c23359e02b7d5d5d31ac2a3fe78a
KIM GYUHO <kyuhokim12@gmail.com>
2023-03-09 22:12:52 +0900

    [Silver II] Title: 제곱수의 합, Time: 32 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define loop(i, s, n) for(int i = s; i <= n; i++)
+#define LOOP(i, s, n) for(int i = s; i < n; i++)
+#define MAXN 100001
+
+using namespace std;
+
+// dp[i] = i를 제곱수들의 합으로 표현할 때 그 항의 최소개수 // dp[j
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, dp[MAXN];
+    cin >> n;
+    dp[1] = 1;
+    loop(i, 1, n)
+        dp[i] = i;
+    loop(i, 1, n)
+        for(int j = 1; j * j <= i; j++)
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+    cout << dp[n];
+}
