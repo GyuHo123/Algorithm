e90393d60ecffb1e51b1693db9fff6255ec169dd
KIM GYUHO <kyuhokim12@gmail.com>
2023-03-19 14:11:58 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define loop(i, s, n) for(int i = s; i <= n; i++)
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    while(1){
+        string str;
+        getline(cin, str);
+        if(str == "") break;
+        cout << str << endl;
+    }
+}
