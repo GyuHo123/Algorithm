2a08d9c7ce0a3e21acf71653b6043a89cdf23e30
KIM GYUHO <kyuhokim12@gmail.com>
2023-03-31 00:08:33 +0900

    [Gold V] Title: 강의실, Time: 48 ms, Memory: 4484 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define loop(i, s, n) for(int i = s; i <= n; i++)
+#define LOOP(i, s, n) for(int i = s; i < n; i++)
+#define ll long long
+#define MAXN 200001
+
+using namespace std;
+
+
+int n;
+pair<int, int> arr[MAXN];
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    loop(i, 0, n - 1){
+        int dump;
+        cin >> dump >> arr[i].first >> arr[i].second;
+    }
+    sort(arr, arr + n);
+    pq.push(arr[0].second);
+    loop(i, 1, n - 1){
+        pq.push(arr[i].second);
+        if(pq.top() <= arr[i].first) pq.pop();
+    }
+    cout << pq.size();
+}
