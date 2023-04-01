66aa867f7168a7a949cef3fb9896167efe8f6fac
KIM GYUHO <kyuhokim12@gmail.com>
2023-04-02 04:27:47 +0900

    [Gold V] Title: 암호코드, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.cc
+++ 백준/Gold/2011. 암호코드/암호코드.cc
@@ -9,29 +9,27 @@
 using namespace std;
 
 string str;
-int temp;
 ll dp[MAXN]; //dp[i] = "i번째에서 가질 수 있는 해석의 가짓수"
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    
+    cin >> str;
 
-    string s;
-    cin >> s;
+    int n = str.length();
 
-    int n = s.length();
-
-    if (s[0] == '0') {
+    if (str[0] == '0') {
         cout << 0 << endl;
-        return 0;
+        exit(0);
     }
 
     dp[0] = 1;
     dp[1] = 1;
 
     loop(i, 2, n){
-        int one_digit = s[i - 1] - '0';
-        int two_digits = (s[i - 2] - '0') * 10 + one_digit;
+        int one_digit = str[i - 1] - '0';
+        int two_digits = (str[i - 2] - '0') * 10 + one_digit;
         if (one_digit == 0 && (two_digits < 10 || two_digits > 26)) {
             cout << 0;
             exit(0);
