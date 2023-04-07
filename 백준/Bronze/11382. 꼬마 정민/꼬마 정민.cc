c16a201eaa0a4cb6a4d53d8ebefdecd44a32537f
KIM GYUHO <kyuhokim12@gmail.com>
2023-04-08 01:56:31 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define loop(i, s, n) for(int i = s; i <= n; i++)
+#define LOOP(i, s, n) for(int i = s; i < n; i++)
+#define MAXN 21
+
+using namespace std;
+
+ll a, b, c;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> a >> b >> c;
+
+    cout << a + b + c;
+}
