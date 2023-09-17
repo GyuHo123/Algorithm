3ad37d7150f9837d21dc75d651ec091e1dba7405
KIM GYUHO <kyuhokim12@gmail.com>
2023-09-17 17:14:15 +0900

    [Gold V] Title: 수 고르기, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -7,27 +7,24 @@
 
 using namespace std;
 
-int n, m, s, e, arr[MAXN];
+int n, m, arr[MAXN], en, ans = INT_MAX;
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> n >> m;
-    int ans = 2000000000;
 
     LOOP(i, 0, n){
         cin >> arr[i];
     }
 
     sort(arr, arr + n);
-    while(s <= e && e < n){
-        int temp = arr[e] - arr[s];
-        if(temp < m) e++;
-        else{
-            ans = min(ans, temp);
-            s++;
-        }
+
+    LOOP(i, 0, n){
+        while(en < n && arr[en] - arr[i] < m) en++;
+        if(en == n) break;
+        ans = min(ans, arr[en] - arr[i]);
     }
-    cout << ans << endl;
+    cout << ans;
 }
