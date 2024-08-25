fc860277298c656d4f0e10893ca937c43ea28c38
KIM GYUHO <kyuhokim12@gmail.com>
2024-08-25 15:58:27 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,40 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define endl '\n'
-#define Mod 1000000000
+#define loop(i, s, n) for(int i = s; i <= n; i++)
+#define ll long long
+#define MAXN 101
+#define MOD 1000000000
+
+int n;
+ll dp[MAXN][10], ans;
 
 using namespace std;
 
-long long dp[101][101];
-long long res;
-
-int main() {
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
-
     cin >> n;
 
-    dp[1][0] = 0;
-
-    for(int i = 1; i <= 9; i++){
+    loop(i, 1, 9){
         dp[1][i] = 1;
     }
 
-    for(int i = 2; i <= n; i++) {
-        for (int j = 0; j <= 9; j++) {
-            if(j == 0) dp[i][j] = dp[i - 1][1] % Mod;
-            if(j == 9) dp[i][j] = dp[i - 1][8] % Mod;
-            else dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % Mod;
+    loop(i, 2, n){
+        dp[i][0] = dp[i - 1][1] % MOD;
+        loop(j, 1, 9){
+            if(j == 9) dp[i][j] = dp[i - 1][j - 1] % MOD;
+            else dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD; 
         }
     }
-
-
-
-    for(int i = 0; i <= 9; i++){
-        res += dp[n][i];
+    loop(i, 0, 9){
+        ans = (ans + dp[n][i]) % MOD;
     }
-
-    cout << res % Mod;
-
+    cout << ans;
 }
