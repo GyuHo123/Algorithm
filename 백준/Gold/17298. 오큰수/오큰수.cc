55195ba24e25076efa4a1185a458fb11bfe5ca36
KIM GYUHO <kyuhokim12@gmail.com>
2024-10-07 21:19:00 +0900

    [Gold IV] Title: 오큰수, Time: 176 ms, Memory: 20308 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,34 +1,35 @@
 #include <bits/stdc++.h>
 #define endl '\n'
-
+#define loop(i, s, n) for(int i = s; i <= n; i++)
+#define ll long long
+#define MAXN 1000001
 using namespace std;
 
-int main() {
+stack<int> st;
+
+ll arr[MAXN], ans[MAXN], n;
+
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    long long n;
+    cin >> n;
 
-    stack<long long> stk;
-
-    cin >> n; long long arr[n], ans[n];
     for(int i = n - 1; i >= 0; i--){
         cin >> arr[i];
     }
-    for(int i = 0; i < n; i++){
-        if(stk.empty()) {
-            stk.push(arr[i]);
+    loop(i, 0, n - 1){
+        if(st.empty()){
+            st.push(arr[i]);
             ans[i] = -1;
         }
         else{
-            while(!stk.empty() && stk.top() <= arr[i]){
-                stk.pop();
+            while(!st.empty() && st.top() <= arr[i]){
+                st.pop();
             }
-            if(stk.empty()) ans[i] = -1;
-            if(!stk.empty() && stk.top() > arr[i]){
-                ans[i] = stk.top();
-            }
-            stk.push(arr[i]);
+            if(st.empty()) ans[i] = -1;
+            else if(!st.empty() && st.top() > arr[i]) ans[i] = st.top();
+            st.push(arr[i]);
         }
     }
     for(int i = n - 1; i >= 0; i--){
